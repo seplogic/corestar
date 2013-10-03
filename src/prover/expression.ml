9a860db07713032f500941f9f62f510c2303096c
Nikos <nikost@eecs.qmul.ac.uk>
2013-10-03 20:21:36 +0100

    Hash-consing

--- src/prover/expression.ml
+++ src/prover/expression.ml
@@ -1,19 +1,50 @@
 open Corestar_std
 open Format
 
-type t = Var of string | App of string * t list
+type t_orig = Var of string | App of string * exp list
+and exp = t_orig * int
   (* TODO: explain which are valid names; empty string is not *)
 
-let mk_app op xs = App (op, xs)
+(* x == y --> x = y *)
+(* x = y --> x == y *)
+
+module ExpBase = Hashtbl.Make (struct
+  type t = exp
+  let hash = snd
+  let equal f1 f2 = match fst f1, fst f2 with
+    | Var x1, Var x2 -> x1 = x2
+    | App (op1, xs1), App (op2, xs2) -> op1 = op2 && xs1 == xs2
+    | _ -> false
+end)
+
+let exp_base = ExpBase.create 0
+
+(* we changed type t to t_orig * int, and hash has type t -> int in the interface;
+   but hash actually computes the int part of e, so it ignores its given one *)
+let hash e =
+  let eh = Hashtbl.hash "" in
+  let f acc x = 31 * acc + (snd x) in
+  match fst e with 
+    | Var v -> 31 * (Hashtbl.hash v) + eh
+    | App (op, xs) -> 31 * (List.fold_left f (Hashtbl.hash op) xs) + eh
+
+let hash_cons f =
+  try ExpBase.find exp_base f
+  with Not_found -> ExpBase.add exp_base f f; f
+
+let mk_app op xs = 
+  let f = (App (op, xs)) in
+  hash_cons (f, hash (f, 666)) (* any simplifications here? *) 
 let mk_var v =
   assert (not (v = "")); (* otherwise, hashes are messed up *)
-  Var v
+  let f = (Var v) in 
+  hash_cons (f, hash (f, 666))
 
-let bk_app = function
+let bk_app f = match fst f with 
   | Var _ -> invalid_arg "bk_app"
   | App (op, xs) -> (op, xs)
 
-let bk_var = function
+let bk_var f = match fst f with
   | Var v -> v
   | App _ -> invalid_arg "bk_var"
 
@@ -41,24 +72,26 @@ let is_vpat p = Str.string_match vpat_re p 0
 
 let eq = (=)  (* TODO: hash-consing *)
 
-let hash e =
-  let eh = Hashtbl.hash "" in
-  let rec h acc = function
-    | Var v -> 31 * (31 * acc + Hashtbl.hash v) + eh
-    | App (v, xs) -> 31 * (List.fold_left h (31 * acc + Hashtbl.hash v) xs) + eh
-  in h 0 e
-
 let vars x =
-  let rec f vs = function
+  let rec f vs exp = match fst exp with
     | Var v -> StringSet.add v vs
     | App (_, xs) -> List.fold_left f vs xs in
   StringSet.elements (f StringSet.empty x)
 
+(* Old code
 let substitute xys =
   let f = ListH.lookup xys in
   let rec r = function
     | Var v as e -> (try f v with Not_found -> e)
     | App (op, xs) -> App (op, List.map r xs) in
+  r
+*)
+
+let substitute xys =
+  let f = ListH.lookup xys in
+  let rec r exp = match fst exp with
+    | Var v -> (try f v with Not_found -> exp)
+    | App (op, xs) -> mk_app op (List.map r xs) in
   r
 
 let mk_0 op = mk_app op []
@@ -85,6 +118,8 @@ let fls = mk_0 "fls"
 
 let rec pp f =
   let pp_rec f e = fprintf f " %a" pp e in
-  function
+  fun g -> match fst g with
     | Var v -> pp_string f v
     | App (op, xs) -> fprintf f "(%s%a)" op (pp_list pp_rec) xs
+
+type t = exp
