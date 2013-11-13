40f38fa0d9a04052cce3d8808786b3cd50eae0a6
Rasmus Lerchedahl Petersen <rusmus@eecs.qmul.ac.uk>
2013-11-13 21:37:09 +0000

    Implemented foldri since it is used in jstar

--- src/utils/corestar_std.ml
+++ src/utils/corestar_std.ml
@@ -148,11 +148,10 @@ module ListH = struct
     let g (acc, i) x = (f acc i x, i + 1) in
     fst (List.fold_left g (z, 0) xs)
   
-  (* TODO: hm.. *)
-  let foldri f xs z = failwith "TODO"
-(*    let g x (i, acc) = (i + 1, f i x acc) in
-    snd (List.fold_right g xs (0, z))
-*)
+  let foldri f xs z =
+    let g x (i, acc) = (i - 1, f i x acc) in
+    snd (List.fold_right g xs (List.length xs - 1, z))
+
   let mapi f xs =
     let g ys i x = f i x :: ys in
     foldli g [] xs
