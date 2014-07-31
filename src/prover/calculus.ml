5cd4c3a53de53f7b2c140cba7c8087d2dd8eb1eb
Jules Villard <j.villard@imperial.ac.uk>
2014-07-31 22:00:01 +0100

    move ops from {core,calculus} to {coreOps, calculusOps}

--- src/prover/calculus.ml
+++ src/prover/calculus.ml
@@ -13,11 +13,6 @@ let rule_abduct = 1 lsl 1
 let rule_inconsistency = 1 lsl 2
 let rule_instantiation = 1 lsl 3
 
-let is_abduct_rule f = f land rule_abduct <> 0
-let is_inconsistency_rule f = f land rule_inconsistency <> 0
-let is_no_backtrack_rule f = f land rule_no_backtrack <> 0
-let is_instantiation_rule f = f land rule_instantiation <> 0
-
 (** default priority for user rules *)
 let default_rule_priority = 10000
 
@@ -53,27 +48,3 @@ type rule_schema =
 | Rewrite_rule of rewrite_schema
 
 type t = rule_schema list
-
-let mk_equiv_rule name priority flags lhs rhs =
-  let f = Syntax.mk_fresh_bool_tpat "_f" in
-  let lo = Syntax.mk_fresh_bool_tpat "_lo" in
-  let rs =
-    [{ seq_name = name ^ "_right"
-     ; seq_pure_check = []
-     ; seq_fresh_in_expr = []
-     ; seq_goal_pattern = { frame = f; hypothesis = lo; conclusion = lhs }
-     ; seq_subgoal_pattern = [{ frame = f; hypothesis = lo; conclusion = rhs }]
-     ; seq_priority = priority
-     ; seq_flags = flags };
-     { seq_name = name ^ "_left"
-     ; seq_pure_check = []
-     ; seq_fresh_in_expr = []
-     ; seq_goal_pattern = { frame = f; hypothesis = lhs; conclusion = lo }
-     ; seq_subgoal_pattern = [{ frame = f; hypothesis = rhs; conclusion = lo }]
-     ; seq_priority = priority
-     ; seq_flags = flags }] in
-  List.map (fun r -> Sequent_rule r) rs
-
-let sequent_equal { frame = f1; hypothesis = h1; conclusion = c1 }
-    { frame = f2; hypothesis = h2; conclusion = c2 } =
-  Syntax.expr_equal f1 f2 && Syntax.expr_equal h1 h2 && Syntax.expr_equal c1 c2
